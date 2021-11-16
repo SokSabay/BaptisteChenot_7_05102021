aae4044e86b1f719ae18cc842485255193f8a939
SokSabay <baptiste.chenot@gmail.com>
2021-11-16 15:02:26 +0100

    ajout middleware auth

--- routes/message.js
+++ routes/message.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const messageCtrl = require("../controllers/message");
+const auth = require("../middleware/auth");
 
-router.post("/", messageCtrl.addMessage);
-router.get("/", messageCtrl.getAllMessage);
-router.get("/post/:id", messageCtrl.getMessagePost);
-router.delete("/:id", messageCtrl.deleteComment);
-router.put("/:id", messageCtrl.modifyComment);
+router.post("/", auth, messageCtrl.addMessage);
+router.get("/", auth, messageCtrl.getAllMessage);
+router.get("/post/:id", auth, messageCtrl.getMessagePost);
+router.delete("/:id", auth, messageCtrl.deleteComment);
+router.put("/:id", auth, messageCtrl.modifyComment);
 
 module.exports = router;
